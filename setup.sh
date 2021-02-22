b6a87a59672c893bfa0630d44f47864bd6847c0c
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-02-22 03:21:12 +0300

    Fix paths

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cd ~
 
 configs_repo=https://github.com/andre487/dotfiles.git
 push_remote=git@github.com:andre487/dotfiles.git
-configs_dir=.dotfiles
+configs_dir="$HOME/.dotfiles"
 
-# Checkout configs
+# Get up-to-date repo data
 if [[ -d "$configs_dir" ]]; then
     cd "$configs_dir"
     cur_branch="$(git branch | grep '\*' | sed -E 's/[* ]+//g')"
@@ -14,12 +13,12 @@ if [[ -d "$configs_dir" ]]; then
     git stash
     git pull --rebase origin "$cur_branch"
     git stash apply || true
-    cd - > /dev/null
 else
     git clone "$configs_repo" "$configs_dir"
     cd "$configs_dir"
     git remote add upstream "$push_remote"
 fi
+cd - > /dev/null
 
 # Create config files
 IFS=$'\n' read -r -d '' -a config_files < <(find "$configs_dir" -maxdepth 1 -type f -name "\.*" -exec basename {} \; && printf '\0')
@@ -33,14 +32,14 @@ for file in "${config_files[@]}"; do
 done
 
 # Install FZF
-fzf_dir=.fzf
+fzf_dir="$HOME/.fzf"
 if ! which fzf &> /dev/null; then
     git clone --depth 1 https://github.com/junegunn/fzf.git "$fzf_dir"
-    "$HOME/$fzf_dir/install" --key-bindings --completion --no-update-rc
+    "$fzf_dir/install" --key-bindings --completion --no-update-rc
 fi
 
 # Install Oh my ZSH
-ohmyzsh_dir=.oh-my-zsh
+ohmyzsh_dir="$HOME/.oh-my-zsh"
 if [[ ! -d "$ohmyzsh_dir" ]]; then
     export KEEP_ZSHRC=yes
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
