658203f227f9de39cc45ad893bb78baecec5dd3f
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-02-22 03:32:38 +0300

    Remove OMZ dir if install fails

--- setup.sh
+++ setup.sh
@@ -44,6 +44,7 @@ if [[ ! -d "$ohmyzsh_dir" ]]; then
     export KEEP_ZSHRC=yes
     if ! sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended; then
         echo "WARNING! Oh my ZSH is not installed"
+        rm -rf "$ohmyzsh_dir"
     fi
 fi
 
