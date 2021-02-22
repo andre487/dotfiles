df666056a32b8b4a990e2cbba92728012682971d
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-02-22 03:22:47 +0300

    Check OMZ setup status

--- setup.sh
+++ setup.sh
@@ -42,7 +42,9 @@ fi
 ohmyzsh_dir="$HOME/.oh-my-zsh"
 if [[ ! -d "$ohmyzsh_dir" ]]; then
     export KEEP_ZSHRC=yes
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+    if [[ ! sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended ]]; then
+        echo "WARNING! Oh my ZSH is not installed"
+    fi
 fi
 
 # Install Vim plugins
